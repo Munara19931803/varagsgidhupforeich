6150ecbfeaa5a553d1042d39b7fb6af5647e7111
Munar <muniaralbekkyzy@gmail.com>
2023-03-24 22:03:10 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+}
+//        mas("");
+//    }
+//    static void mas(String name ) {
+//        Scanner scanner = new Scanner(System.in);
+//        String text = scanner.nextLine();
+//        System.out.println(text.toUpperCase(Locale.ROOT));    //чон тамга кылып берет
+//        System.out.println(text.substring(3, 4));
+//        if (text .contains("a")){
+//            System.out.println("бар");
+//        }else {
+//            System.out.println("жок");
+//        }
+//
+//    }
+//}
+//        vas("Aidana", "Jumadil", "Meder", "Albina");
+//    }
+//
+//    static void vas(String... a) {
+//        for (String i : a) {
+//            System.out.println(i + "   орозонуз кабыл болсун  ");
+//        }
+//
+//    }
+//}
+
+//    static int method(int... a) {
+//        int result = 0;
+//        for (int i = 0; i < a.length; i++) {
+//            result += a[i];
+//        }
+//        return result;
+//    }
+//
+//}
